3b0fae206f4c984cb5a2591220c110bf9beb92d3
PingHanLuo <Robinpluo@gmail.com>
2016-12-27 16:40:33 -0500

    Started basis for web app game

---
+++ Sheriff.js
@@ -0,0 +1,16 @@
+//Robin Luo
+//Sheriff of Nottingham game
+//Interactive web app to run Sheriff of Nottingham
+
+var url = require('fs');
+var express = require("express");
+var sheriff = express();
+
+//sheriff actions
+sheriff.post('/check',function(){
+});
+
+//smuggler actions
+//bribe
+sheriff.post('/bribe',function(){
+});
---
+++ public/sheriffClient.js
@@ -0,0 +1,16 @@
+$(Document).ready(function(){
+	//buttons from UI
+});
+function update(){
+	$.ajax{
+		method:"GET",
+		url:"/update",
+		datatype:'json',
+		success:function(data){
+			//update info
+		}
+	}
+}
+//sheriff functions
+function bribe(){
+}
